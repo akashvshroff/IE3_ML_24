9c246c795d62f857d6d6e9638cee5d8f5fb8b497
mikeyL64 <michaellee32164@gmail.com>
2024-04-26 17:49:58 -0500

    Fixed coloring, added drawn landmarks

--- DemoApp/app.py
+++ DemoApp/app.py
@@ -1,6 +1,13 @@
 import cv2
 import tempfile
 import streamlit as st
+import mediapipe as mp
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
+
+mp_drawing = mp.solutions.drawing_utils
+mp_hands = mp.solutions.hands
+hand = mp_hands.Hands()
 
 def main():
     # Title
@@ -45,11 +52,24 @@ def main():
     st.sidebar.text('Input Video')
     st.sidebar.video(tffile.name)
 
+    # holistic = mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5)
+
+
     while vid.isOpened():
         ret, image = vid.read()
 
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
         if not ret:
             break
+
+        results = hand.process(image)
+
+        if results.multi_hand_landmarks:
+
+            for hand_landmarks in results.multi_hand_landmarks:
+                print(hand_landmarks)
+                mp_drawing.draw_landmarks(image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
 
         stframe.image(image, use_column_width=True)
 
